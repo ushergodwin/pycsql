aac56d67819c3801d260821189f123c077dddfc8
= <=>
2021-07-18 17:23:58 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='pysql',
+version='1.0',
+description='Execute custom queries with either pymysql, mysqlclient or mysql connector',
+url='https://github.com/ushergodwin/pysql.git`',
+author='Tumuhimbis Usher Godwin',
+author_email='godwintumuhimbise96@email.com',
+license='MIT',
+packages=['pysql'],
+zip_safe=False)
