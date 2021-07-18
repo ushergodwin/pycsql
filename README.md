68fd554e3fee96f36fc555aff7b50e7698e93f82
= <=>
2021-07-18 18:24:04 +0300

    packaged

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pysql
-A Custom SQL Database Client to run database transactions. Can work with both mysqlclient, pymysql and msqlconnector
+MYSQL Database package to run database transactions. Can work with both mysqlclient, pymysql and msqlconnector
+# Required Packages
+- Any Mysql Client package
+- decouple
+# Optional Package
+- Django
+# Configuration
+- Use the .enve.example template for your database configurations
--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
-from setuptools import setup
-setup(name='pysql',
-version='1.0',
-description='Execute custom queries with either pymysql, mysqlclient or mysql connector',
-url='https://github.com/ushergodwin/pysql.git`',
-author='Tumuhimbis Usher Godwin',
-author_email='godwintumuhimbise96@email.com',
-license='MIT',
-packages=['pysql'],
-zip_safe=False)
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pysql-pkg-ushergodwin",
+    version="0.0.1",
+    author="Tumuhimbise Usher Godwin",
+    author_email="godwintumuhimbise96.com",
+    description="A lightweight mysql package that works with either pymysql, mysqlclient or mysql connectore",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ushergodwin/pysql",
+    project_urls={
+        "Bug Tracker": "https://github.com/ushergodwin/pysql/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+)
